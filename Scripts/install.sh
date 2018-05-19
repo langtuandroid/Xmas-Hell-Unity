ae97d3f85d6729662ccc739a1ea686e074c5fe79
Noxalus <noxalus@gmail.com>
2018-05-19 12:20:51 +0200

    Update install.sh script to install Android support for Unity.

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -27,5 +27,5 @@ install()
 
 install "MacEditorInstaller/Unity-$VERSION.pkg"
 install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-$VERSION.pkg"
 install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
-install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
