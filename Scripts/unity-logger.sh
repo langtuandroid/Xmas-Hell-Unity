324246235dc4af62372890394af338f5052799dd
Noxalus <noxalus@gmail.com>
2018-05-19 18:15:30 +0200

    Update unity-logger file.

--- Scripts/unity-logger.sh
+++ Scripts/unity-logger.sh
@@ -1,7 +1,17 @@
- #!/bin/sh
+#!/bin/sh
 
- tail -F ~/Library/Logs/Unity/Editor.log &
- /Applications/Unity/Unity.app/Contents/MacOS/Unity "$@"
- EXITCODE="$?"
- kill %1
- exit "$EXITCODE"
+UNITY_LOG_PATH="~/Library/Logs/Unity"
+UNITY_LOG_FILE="Editor.log"
+
+echo "Unity log path: $UNITY_LOG_PATH"
+
+if [[ ! -e ${UNITY_LOG_PATH}/${UNITY_LOG_FILE} ]]; then
+  mkdir -p ${UNITY_LOG_PATH}
+  touch ${UNITY_LOG_PATH}/${UNITY_LOG_FILE}
+fi
+
+tail -F ${UNITY_LOG_PATH} &
+/Applications/Unity/Unity.app/Contents/MacOS/Unity "$@"
+EXITCODE="$?"
+kill %1
+exit "$EXITCODE"
