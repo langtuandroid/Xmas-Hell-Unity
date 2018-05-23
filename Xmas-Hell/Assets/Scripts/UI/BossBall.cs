5b22a289574448277d506f9051851d893bd57fa7
Noxalus <noxalus@gmail.com>
2018-05-23 21:13:54 +0200

    Update BossBall script to vary jiggle animation.

--- Xmas-Hell/Assets/Scripts/UI/BossBall.cs
+++ Xmas-Hell/Assets/Scripts/UI/BossBall.cs
@@ -3,6 +3,13 @@
 public class BossBall : MonoBehaviour
 {
     public EBoss BossType;
+    public Animator Animator;
+
+    public void Start()
+    {
+        Animator.speed = Random.Range(0.5f, 1.5f);
+        Animator.Play("Jiggle", 0, Random.Range(0f, 1f));
+    }
 
     public void OnClick()
     {
