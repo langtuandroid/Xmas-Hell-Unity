66900cc7e7de7255bfae597804f50fd855e0c4fc
Noxalus <noxalus@gmail.com>
2018-05-26 13:58:31 +0200

    Add a script to make clouds on the menu screen cross the screen randomly.

---
+++ Xmas-Hell/Assets/Scripts/Menu/MenuCloud.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MenuCloud : MonoBehaviour
+{
+    // TODO: Find a way to display RandomRange properly in the editor
+    public float RandomSpeedRangeMin;
+    public float RandomSpeedRangeMax;
+    public float RandomVerticalPositionRangeMin;
+    public float RandomVerticalPositionRangeMax;
+    public float RandomDelayMin;
+    public float RandomDelayMax;
+
+    private float _speedFactor;
+
+	void Start ()
+    {
+        StartCoroutine("CrossScreenHorizontaly");
+    }
+
+    IEnumerator CrossScreenHorizontaly()
+    {
+        var factor = Random.value < .5 ? 1 : -1;
+        var newPosition = transform.position;
+
+        _speedFactor = Random.Range(RandomSpeedRangeMin, RandomSpeedRangeMax) * factor;
+
+        newPosition.x = (factor < 0) ? 10 : -10;
+        newPosition.y = Random.Range(RandomVerticalPositionRangeMin, RandomVerticalPositionRangeMax);
+        transform.position = newPosition;
+
+        yield return new WaitForSeconds(Random.Range(RandomDelayMin, RandomDelayMax));
+
+        while ((_speedFactor < 0 && transform.position.x > -10) ||
+              (_speedFactor > 0 && transform.position.x < 10))
+        {
+            newPosition.x += _speedFactor * Time.deltaTime;
+            transform.position = newPosition;
+
+            yield return null;
+        }
+
+        StartCoroutine("CrossScreenHorizontaly");
+    }
+}
