84515c7d54fe36d6981e1a7d491ae6c99f9712f2
Noxalus <noxalus@gmail.com>
2018-06-17 14:50:16 +0200

    Start the animations of the Xmas Candy.

---
+++ Xmas-Hell/Assets/Scripts/Player/PlayerFrontAnimator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerFrontAnimator : MonoBehaviour
+{
+    public Animator Animator;
+
+	void Start ()
+    {
+        StartCoroutine("StartRandomAnimationLoop");
+	}
+
+    private IEnumerator StartRandomAnimationLoop()
+    {
+        Animator.Play("CoucouSide", 0, 0);
+
+        while (Animator.GetCurrentAnimatorStateInfo(0).IsName("Idle") || Animator.GetCurrentAnimatorStateInfo(0).normalizedTime < .99f)
+        {
+            yield return null;
+        }
+
+        Animator.Play("Idle");
+        yield return new WaitForSeconds(Random.Range(1, 5));
+        StartCoroutine("StartRandomAnimationLoop");
+    }
+}
