6c3a5d4bd0aedf17967b477de52814984387e26e
Noxalus <noxalus@gmail.com>
2018-06-21 00:33:54 +0200

    Add game music.

---
+++ Xmas-Hell/Assets/Scripts/Audio/GameMusicManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class GameMusicManager : MonoBehaviour
+{
+    public AudioSource Intro;
+    public AudioSource Loop;
+
+    private bool startedLoop = false;
+
+    void Start()
+    {
+        Intro.Play();
+        //StartCoroutine(PlayEngineSound());
+    }
+
+    void FixedUpdate()
+    {
+        if (!Intro.isPlaying && !startedLoop)
+        {
+            Loop.Play();
+            Debug.Log("Done playing");
+            startedLoop = true;
+        }
+    }
+
+    IEnumerator PlayEngineSound()
+    {
+        Intro.Play();
+
+        yield return new WaitForSeconds(Intro.clip.length);
+
+        Loop.Play();
+    }
+}
