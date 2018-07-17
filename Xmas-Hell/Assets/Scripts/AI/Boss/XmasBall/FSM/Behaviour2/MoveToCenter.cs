6e8ff7e05aa6ea0295c02c4e1f38c37c532fb774
Noxalus <noxalus@gmail.com>
2018-07-17 23:19:29 +0200

    Add FSM for the second behaviour of the Xmas Ball.

---
+++ Xmas-Hell/Assets/Scripts/AI/Boss/XmasBall/FSM/Behaviour2/MoveToCenter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace XmasBallBehaviour2FSM
+{
+    public class MoveToCenter : BossStateMachineBehaviour
+    {
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            base.OnStateEnter(animator, stateInfo, layerIndex);
+
+            Boss.Speed = Boss.InitialSpeed * 50f;
+            Boss.MoveTo(Vector2.zero, true);
+        }
+
+        override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            base.OnStateUpdate(animator, stateInfo, layerIndex);
+
+            Debug.Log("Boss position: " + Boss.Position.ToString());
+
+            if ((Mathf.Approximately(Boss.Position.x, 0) && Mathf.Approximately(Boss.Position.y, 0)) ||
+                (Mathf.Abs(Boss.Position.x) < 0.5f && Mathf.Abs(Boss.Position.y) < 0.5f))
+            {
+                animator.SetTrigger("StartCircularPattern");
+            }
+        }
+    }
+}
---
+++ Xmas-Hell/Assets/Scripts/AI/Boss/XmasBall/FSM/Behaviour2/Shoot.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace XmasBallBehaviour2FSM
+{
+    public class Shoot : BossStateMachineBehaviour
+    {
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            base.OnStateEnter(animator, stateInfo, layerIndex);
+
+            Debug.Log("Shoot bullet!");
+        }
+    }
+}
