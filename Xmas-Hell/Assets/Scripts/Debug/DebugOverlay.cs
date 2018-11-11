dc6013631ec4beef328713184e510f40bfe8d272
Noxalus <noxalus@gmail.com>
2018-11-11 14:03:01 +0100

    Add a debug bullet counter in the game scene.

---
+++ Xmas-Hell/Assets/Scripts/Debug/DebugOverlay.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityBulletML.Bullets;
+using UnityEngine;
+
+public class DebugOverlay : MonoBehaviour
+{
+    [Header("Manager references")]
+    [SerializeField] private BulletManager _bulletManager;
+
+    [Header("UI references")]
+    [SerializeField] private TextMeshProUGUI _bulletCounter;
+
+    void Update()
+    {
+        _bulletCounter.text = _bulletManager.Bullets.Count.ToString();
+    }
+}
