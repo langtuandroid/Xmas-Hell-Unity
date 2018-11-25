806f1ed1d31c16afe455ee1c98cb94bde8a0fd4f
Noxalus <noxalus@gmail.com>
2018-11-25 18:44:44 +0100

    Started to implement the 4th Xmas Snowflake pattern.

---
+++ Xmas-Hell/Assets/Project/Scripts/AI/Boss/XmasSnowflake/XmasSnowflakeBehaviour4.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityBulletML.Bullets;
+using UnityEngine;
+
+public class XmasSnowflakeBehaviour4 : AbstractBossBehaviour
+{
+    [SerializeField] private List<BulletEmitter> _bulletEmitters;
+
+    private List<BulletEmitter> _currentBulletEmitters;
+
+    public override void StartBehaviour()
+    {
+        base.StartBehaviour();
+
+        // Save the previous boss bullet emitters
+        _currentBulletEmitters = Boss.GetBulletEmitters();
+
+        // Replace boss bullet emitters
+        Boss.SetBulletEmitters(_bulletEmitters);
+    }
+
+    public override void StopBehaviour()
+    {
+        base.StopBehaviour();
+
+        // Don't forget to reapply the boss previous bullet emitters
+        Boss.SetBulletEmitters(_currentBulletEmitters);
+    }
+}
