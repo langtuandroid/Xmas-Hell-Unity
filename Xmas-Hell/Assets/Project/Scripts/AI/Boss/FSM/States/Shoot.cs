3e86b873f30fdc571f90fe552b792d9de2f32792
Noxalus <noxalus@gmail.com>
2018-11-25 19:37:41 +0100

    Update the Shoot FSM state script to be able to specify a shoot frequency.

--- Xmas-Hell/Assets/Project/Scripts/AI/Boss/FSM/States/Shoot.cs
+++ Xmas-Hell/Assets/Project/Scripts/AI/Boss/FSM/States/Shoot.cs
@@ -6,10 +6,29 @@ namespace BossBehaviourState
     {
         public string PatternName = "default";
 
+        [Header("Frequency")]
+        public float ShootFrequency = 0f;
+
         override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
             base.OnStateEnter(animator, stateInfo, layerIndex);
 
+            if (ShootFrequency == 0)
+                Boss.ShootPattern(PatternName);
+            else
+                Boss.StartShootTimer(ShootFrequency, ShootTimerFinished);
+        }
+
+        override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            base.OnStateExit(animator, stateInfo, layerIndex);
+
+            if (ShootFrequency > 0)
+                Boss.StopShootTimer();
+        }
+
+        private void ShootTimerFinished()
+        {
             Boss.ShootPattern(PatternName);
         }
     }
