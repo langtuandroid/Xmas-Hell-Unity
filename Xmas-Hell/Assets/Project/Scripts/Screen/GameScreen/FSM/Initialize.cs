63fd9e47ff2d58b6129621248d6cbf2b462b44bf
Noxalus <noxalus@gmail.com>
2018-11-27 09:08:03 +0100

    Fix issue where bullet pattern files were not loaded anymore.

--- Xmas-Hell/Assets/Project/Scripts/Screen/GameScreen/FSM/Initialize.cs
+++ Xmas-Hell/Assets/Project/Scripts/Screen/GameScreen/FSM/Initialize.cs
@@ -8,8 +8,8 @@ namespace GameScreenFSM
         {
             base.OnStateEnter(animator, stateInfo, layerIndex);
 
-            GameManager.Initialize();
             GameManager.LoadBoss(SessionData.SelectedBoss);
+            GameManager.Initialize();
         }
 
         public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
