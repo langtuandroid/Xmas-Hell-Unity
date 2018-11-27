4efa76794a43ccffc36ffdcacc493ff70aee340a
Noxalus <noxalus@gmail.com>
2018-11-27 14:01:49 +0100

    Add a script to connect to the Google Play Games Services.

---
+++ Xmas-Hell/Assets/Project/Scripts/PlayGamesServices.cs
@@ -0,0 +1,29 @@
+﻿using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
+
+public class PlayGamesServices : MonoBehaviour
+{
+    void Start()
+    {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.DebugLogEnabled = Debug.isDebugBuild;
+        PlayGamesPlatform.Activate();
+
+        SignIn();
+    }
+
+    public void SignIn()
+    {
+        Social.localUser.Authenticate((bool success) => 
+        {
+            Debug.Log("User logged in with GPGS.");
+        });
+    }
+
+    public void SignOut()
+    {
+        PlayGamesPlatform.Instance.SignOut();
+    }
+}
