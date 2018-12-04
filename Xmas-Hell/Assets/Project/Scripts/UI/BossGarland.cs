be250a17cae618eb7fc31aae2162a63fa91c5b4f
Noxalus <noxalus@gmail.com>
2018-12-04 23:06:52 +0100

    Add a boss garland script to change garland color according to game progress.

---
+++ Xmas-Hell/Assets/Project/Scripts/UI/BossGarland.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BossGarland : MonoBehaviour
+{
+    [SerializeField] private EBoss _relatedBoss;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private Image _image;
+
+    private EBossBallState _state = EBossBallState.Unknown;
+
+    public void Start()
+    {
+        CheckState();
+    }
+
+    private void CheckState()
+    {
+        var bossData = SaveSystem.GetBossData(_relatedBoss);
+        var bossState = EBossBallState.Unknown;
+
+        if (bossData.WinCounter > 0)
+        {
+            bossState = EBossBallState.Beaten;
+        }
+
+        SetState(bossState);
+    }
+
+    public void SetState(EBossBallState state)
+    {
+        _state = state;
+
+        if (_state == EBossBallState.Beaten)
+        {
+            _image.color = Color.green;
+        }
+        else
+        {
+            _image.color = Color.red;
+        }
+    }
+}
