6d2f13de0487ffc5540b90d702843e4f325c5d6c
Noxalus <noxalus@gmail.com>
2018-12-05 22:50:08 +0100

    Spawn bullets when Xmas Candy collides with the borders of the screen in its 2nd pattern.

--- Xmas-Hell/Assets/Project/Scripts/AI/Boss/XmasCandy/FSM/States/Extend.cs
+++ Xmas-Hell/Assets/Project/Scripts/AI/Boss/XmasCandy/FSM/States/Extend.cs
@@ -4,15 +4,18 @@ namespace BossBehaviourState
 {
     public class Extend : BossStateMachineBehaviour
     {
-        private bool _revertExtend;
         private Animator _currentAnimator;
 
         override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
             base.OnStateEnter(animator, stateInfo, layerIndex);
 
-            _revertExtend = false;
             _currentAnimator = animator;
+
+            if (_currentAnimator.GetBool("ReverseExtend"))
+                _currentAnimator.SetFloat("ExtendSpeedMultiplier", -1f);
+            else
+                _currentAnimator.SetFloat("ExtendSpeedMultiplier", 1f);
 
             Boss.OnCollision.AddListener(OnCollision);
         }
@@ -21,24 +24,25 @@ namespace BossBehaviourState
         {
             base.OnStateUpdate(animator, stateInfo, layerIndex);
 
-            if (_revertExtend && _currentAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime <= 0 && 
+            if (_currentAnimator.GetBool("ReverseExtend") && 
+                _currentAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime <= 0 && 
                 !_currentAnimator.IsInTransition(0))
             {
                 _currentAnimator.SetTrigger("ExtendFinished");
                 _currentAnimator.SetFloat("ExtendSpeedMultiplier", 1f);
+                _currentAnimator.SetBool("ReverseExtend", false);
             }
         }
 
         private void OnCollision(Collision2D collision)
         {
-            if (_revertExtend)
+            if (_currentAnimator.GetBool("ReverseExtend"))
                 return;
 
             if (collision.gameObject.tag == "Wall")
             {
-                Boss.GameManager.CameraManager.Shake(0.5f, 0.5f);
-                _revertExtend = true;
-                _currentAnimator.SetFloat("ExtendSpeedMultiplier", -1f);
+                _currentAnimator.SetTrigger("OnWallCollision");
+                _currentAnimator.SetBool("ReverseExtend", true);
             }
         }
 
