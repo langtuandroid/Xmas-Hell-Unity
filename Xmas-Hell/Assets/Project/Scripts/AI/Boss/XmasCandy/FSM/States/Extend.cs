53cc2a8159522c1d19dc574d85dfc0b8fb08aefd
Noxalus <noxalus@gmail.com>
2018-12-05 22:41:12 +0100

    Fix the extend part of the Xmas Candy's second pattern.

---
+++ Xmas-Hell/Assets/Project/Scripts/AI/Boss/XmasCandy/FSM/States/Extend.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+namespace BossBehaviourState
+{
+    public class Extend : BossStateMachineBehaviour
+    {
+        private bool _revertExtend;
+        private Animator _currentAnimator;
+
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            base.OnStateEnter(animator, stateInfo, layerIndex);
+
+            _revertExtend = false;
+            _currentAnimator = animator;
+
+            Boss.OnCollision.AddListener(OnCollision);
+        }
+
+        public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            base.OnStateUpdate(animator, stateInfo, layerIndex);
+
+            if (_revertExtend && _currentAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime <= 0 && 
+                !_currentAnimator.IsInTransition(0))
+            {
+                _currentAnimator.SetTrigger("ExtendFinished");
+                _currentAnimator.SetFloat("ExtendSpeedMultiplier", 1f);
+            }
+        }
+
+        private void OnCollision(Collision2D collision)
+        {
+            if (_revertExtend)
+                return;
+
+            if (collision.gameObject.tag == "Wall")
+            {
+                Boss.GameManager.CameraManager.Shake(0.5f, 0.5f);
+                _revertExtend = true;
+                _currentAnimator.SetFloat("ExtendSpeedMultiplier", -1f);
+            }
+        }
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            base.OnStateExit(animator, stateInfo, layerIndex);
+
+            Boss.OnCollision.RemoveListener(OnCollision);
+        }
+    }
+}
